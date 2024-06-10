0ce327c8a6eb660982cbc1e9539d8d2203baf806
ollie <ollie@ollierobinson.co.uk>
2024-06-10 13:23:16 +0100

    begin high-level API to mirror C++ api

---
+++ MixrSharp/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static MixrSharp.MixrNative;
+
+namespace MixrSharp;
+
+public class Context : IDisposable
+{
+    private nint _context;
+
+    public Context(uint sampleRate)
+    {
+        mxCreateContext(sampleRate, out _context);
+    }
+
+    public Context(IntPtr context)
+    {
+        _context = context;
+    }
+
+    public void Dispose()
+    {
+        mxDestroyContext(_context);
+    }
+}
